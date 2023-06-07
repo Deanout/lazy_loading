afffee1d12e5d6eb48030c5cfe7afba8803d8d25
Dean DeHart <DeHartDean@gmail.com>
2023-06-07 15:53:17 -0400

    Init commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def lazy_load
+    sleep(2)
+    # @posts = Post.all
+    # Limit @posts to 10 posts
+    @posts = Post.limit(10)
+    render partial: "posts/posts", posts: @posts
+  end
+end
